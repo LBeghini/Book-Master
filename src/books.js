259e914e6110d8788d3f9ad15e944146e82bb533
Lavínia Beghini <lavinia.beghini@gmail.com>
2020-10-02 18:34:00 -0300

    Create book array

---
+++ src/books.js
@@ -0,0 +1,8 @@
+const books = [
+    {id: 1, title: 'Book1', author: 'author', year:2000},
+    {id: 2, title: 'Book2', author: 'author', year:2000},
+    {id: 2, title: 'Book2', author: 'author', year:2000},
+    {id: 3, title: 'Book3', author: 'author', year:2000}
+]
+
+export default books;
